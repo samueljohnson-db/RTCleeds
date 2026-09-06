d20e4cd4322d71c1680a13f00118a349f60be035
samuel johnson <115143553+samueljohnson-db@users.noreply.github.com>
2026-09-06 23:30:12 +0100

    Update 03_cleaning_data.sql

--- sql/transformations/03_cleaning_data.sql
+++ sql/transformations/03_cleaning_data.sql
@@ -2,3 +2,31 @@
 
 
 -- this code changes incidents joined table to make it uniform for the weather data to join --
+ALTER TABLE STAGING_INCIDENT_JOINED
+MODIFY FULL_TIME VARCHAR2(8);
+
+UPDATE STAGING_INCIDENT_JOINED
+SET FULL_TIME =
+    LPAD(
+        SUBSTR(
+            LPAD(REPLACE(FULL_TIME, ':', ''), 4, '0'),
+            1, 2
+        ), 2, '0'
+    )
+    || ':' ||
+    LPAD(
+        SUBSTR(
+            LPAD(REPLACE(FULL_TIME, ':', ''), 4, '0'),
+            3, 2
+        ), 2, '0'
+    );
+select * from STAGING_INCIDENT_JOINED
+ALTER TABLE STAGING_INCIDENT_JOINED
+MODIFY FULL_DATE DATE;
+UPDATE STAGING_INCIDENT_JOINED
+SET FULL_DATE = TO_DATE(FULL_DATE, 'DD/MM/YYYY');
+SELECT
+    TO_CHAR(FULL_DATE, 'DD/MM/YYYY') AS FULL_DATE
+FROM STAGING_INCIDENT_JOINED;
+UPDATE STAGING_INCIDENT_JOINED
+SET FULL_DATE = TO_DATE(FULL_DATE, 'DD/MM/RR');
