bc2ba9e0df66d49ba8bdf94f6ccc6879fc2b7832
samuel johnson <115143553+samueljohnson-db@users.noreply.github.com>
2026-09-06 23:29:59 +0100

    Update 03_cleaning_data.sql

--- sql/transformations/03_cleaning_data.sql
+++ sql/transformations/03_cleaning_data.sql
@@ -1,1 +1,4 @@
 -- file containing processes within an ETL stage that will clean the original loaded data to make it Uniform --
+
+
+-- this code changes incidents joined table to make it uniform for the weather data to join --
